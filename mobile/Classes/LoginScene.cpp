14725ea9bd47084a8cbc6b393346ace75b21d170
unknown <tranvanbinh.leo@gmail.com>
2017-12-11 16:22:34 -0800

    add login

---
+++ mobile/Classes/LoginScene.cpp
@@ -0,0 +1,53 @@
+#include "LoginScene.h"
+#include "SimpleAudioEngine.h"
+#include "cocostudio\CocoStudio.h"
+
+USING_NS_CC;
+
+Scene* LoginScene::createScene()
+{
+    return LoginScene::create();
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+    printf("Error while loading: %s\n", filename);
+    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in LoginSceneScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool LoginScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	auto rootNode = CSLoader::createNode("LoginScene.csb");
+
+    return true;
+}
+
+
+//void LoginScene::menuCloseCallback(Ref* pSender)
+//{
+//    //Close the cocos2d-x game scene and quit the application
+//    Director::getInstance()->end();
+//
+//    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+//    exit(0);
+//#endif
+//
+//    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
+//
+//    //EventCustom customEndEvent("game_scene_close_event");
+//    //_eventDispatcher->dispatchEvent(&customEndEvent);
+//
+//
+//}
